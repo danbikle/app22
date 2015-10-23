969de679c9bdeb35a184035d298a65c65b4fd6e7
Dan Bikle <dan.bikle@gmail.com>
2015-10-23 01:09:42 +0000

    mo

---
+++ haml/demo.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# /home/ann/app22/haml/demo.bash
+
+.  /home/ann/app22/app22env.bash
+cd /home/ann/app22/haml/
+
+~ann/app22/gems/bin/haml -eq ~ann/app22/haml/temp.haml ~ann/app22/temp.html
+
+exit
